3023e08dba175faf86cc6b01844fb8393027d36f
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-02 07:54:35 +0330

    Function: totalBirdCount

--- bird-watcher.js
+++ bird-watcher.js
@@ -1,1 +1,13 @@
 // this comment for me --->> please look back again 🙂😶😉
+
+// const birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+
+function totalBirdCount(birdsPerDay) {
+  let sum = 0;
+  for (let index = 0; index < birdsPerDay.length; index++) {
+    sum += birdsPerDay[index];
+  }
+  return sum;
+}
+
+// console.log(totalBirdCount(birdsPerDay));
